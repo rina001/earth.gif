43fa1bcd0612680448347f695f9822b52dbfbe5a
rina001 <rinaqiu88@yahoo.com>
2018-02-26 22:30:12 -0800

    Commit this earth to github

---
+++ earth_gif.R
@@ -0,0 +1,8 @@
+earth <- image_read("https://jeroen.github.io/images/earth.gif") %>%
+  image_scale("200x") %>%
+  image_quantize(128)
+length(earth)
+head(image_info(earth))
+rev(earth) %>%
+  image_flip() %>%
+  image_annotate("meanwhile in USA",size = 20,color = "white")
